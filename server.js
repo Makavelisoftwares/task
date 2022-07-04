0924579cebbc11308f8d50a14048425deb8aae80
makavelisoftwares <makavelisoftwares@gmail.com>
2022-07-04 11:18:24 +0300

    change6

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express()
 
 // setting view engines and middlewares
 app.set('view engine','ejs');
+app.use(express.static('public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
